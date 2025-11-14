5c53d17a057b44be3a8f0b8add8b910204f4e0b8
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2025-11-14 20:28:09 +0530

    basics of javascript

---
+++ JavaScript/index.js
@@ -0,0 +1,24 @@
+var a = 10;
+var a = 20;
+
+let b = 30;
+
+console.log(a, b);
+
+let ar = ["abc", 12345, 45.74, "a", true];
+console.log(ar);
+
+console.error("This is an error");
+console.warn("This is warning");
+
+alert("This is a alert");
+
+
+
+if(confirm("Are u sure?")){
+    console.log("thanks")
+}
+else{
+    console.error("it's ok")
+}
+
