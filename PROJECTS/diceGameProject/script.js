36439a48407e93560e2168878ecd0bc102c9e699
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2025-11-28 18:32:18 +0530

    dice game formation

--- PROJECTS/diceGameProject/script.js
+++ PROJECTS/diceGameProject/script.js
@@ -19,42 +19,51 @@ function p1Play() {
   const DF = Math.floor(Math.random() * 6) + 1;
 
   switch (DF) {
-    case 1: {document.getElementById("p1dice").src="/PROJECTS/diceGameProject/images/image1.png";
-    break;
-   }
-    case 2: {document.getElementById("p1dice").src="/PROJECTS/diceGameProject/images/image2.png";
-    break;
-   }
-    case 3: {document.getElementById("p1dice").src="/PROJECTS/diceGameProject/images/image3.png";
-    break;
-   }
-    case 4: {document.getElementById("p1dice").src="/PROJECTS/diceGameProject/images/image4.png";
-    break;
-   }
-    case 5: {document.getElementById("p1dice").src="/PROJECTS/diceGameProject/images/image5.png";
-    break;
-   }
-    case 6: {document.getElementById("p1dice").src="/PROJECTS/diceGameProject/images/image6.png";
-    break;
-   }
-    default:{document.getElementById("p1dice").src="/PROJECTS/diceGameProject/images/image6.png";
-    break;
-   }
-}
-
+    case 1: {
+      document.getElementById("p1dice").src =
+        "/PROJECTS/diceGameProject/images/image1.png";
+      break;
+    }
+    case 2: {
+      document.getElementById("p1dice").src =
+        "/PROJECTS/diceGameProject/images/image2.png";
+      break;
+    }
+    case 3: {
+      document.getElementById("p1dice").src =
+        "/PROJECTS/diceGameProject/images/image3.png";
+      break;
+    }
+    case 4: {
+      document.getElementById("p1dice").src =
+        "/PROJECTS/diceGameProject/images/image4.png";
+      break;
+    }
+    case 5: {
+      document.getElementById("p1dice").src =
+        "/PROJECTS/diceGameProject/images/image5.png";
+      break;
+    }
+    case 6: {
+      document.getElementById("p1dice").src =
+        "/PROJECTS/diceGameProject/images/image6.png";
+      break;
+    }
+    default: {
+      document.getElementById("p1dice").src =
+        "/PROJECTS/diceGameProject/images/image6.png";
+      break;
+    }
+  }
 
   // console.log(DF);
-  if(DF===6)
-  {
+  if (DF === 6) {
     document.getElementById("rollDice1").disabled = true;
     document.getElementById("rollDice2").disabled = false;
-
+  } else {
+    score = score + DF;
+    document.getElementById("p1sc").innerText = score;
   }
-  else{
-        score = score + DF;
-        document.getElementById("p1sc").innerText = score;
-  }
-  
 }
 
 function p2Play() {
@@ -63,46 +72,39 @@ function p2Play() {
 
   const DF = Math.floor(Math.random() * 6) + 1;
 
-//   switch (DF) {
-//     case 1: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
-//     break;
-//    }
-//     case 2: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
-//     break;
-//    }
-//     case 3: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
-//     break;
-//    }
-//     case 4: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
-//     break;
-//    }
-//     case 5: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
-//     break;
-//    }
-//     case 6: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
-//     break;
-//    }
-//     default:{document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
-//     break;
-//    }
+  //   switch (DF) {
+  //     case 1: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
+  //     break;
+  //    }
+  //     case 2: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
+  //     break;
+  //    }
+  //     case 3: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
+  //     break;
+  //    }
+  //     case 4: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
+  //     break;
+  //    }
+  //     case 5: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
+  //     break;
+  //    }
+  //     case 6: {document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
+  //     break;
+  //    }
+  //     default:{document.getElementById("p2dice").src="/PROJECTS/diceGameProject/image.png";
+  //     break;
+  //    }
 
-
-
-    document.getElementById("p2dice").src=`/PROJECTS/diceGameProject/images/image${DF}.png`
+  document.getElementById(
+    "p2dice"
+  ).src = `/PROJECTS/diceGameProject/images/image${DF}.png`;
 
   // console.log(DF);
-  if(DF===6)
-  {
+  if (DF === 6) {
     document.getElementById("rollDice1").disabled = false;
     document.getElementById("rollDice2").disabled = true;
-
+  } else {
+    score = score + DF;
+    document.getElementById("p2sc").innerText = score;
   }
-  else{
-        score = score + DF;
-        document.getElementById("p2sc").innerText = score;
-  }
-  
-
-  }
-
-
+}
