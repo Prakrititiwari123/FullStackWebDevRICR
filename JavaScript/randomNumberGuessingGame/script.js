e1df0f9dba7a98b6c01047290ac3703fa37b4a7f
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2025-11-29 00:05:40 +0530

    GAME-Guessing Number in JS

---
+++ JavaScript/randomNumberGuessingGame/script.js
@@ -0,0 +1,6 @@
+let ans = 25;
+let n = prompt("Guess the number");
+while (n != ans) {
+  n = prompt("You entered wrong number .Guess again: ");
+}
+console.log("Congratulations, you guess the right number");
