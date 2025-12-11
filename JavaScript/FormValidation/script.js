c2945c3100d3840f8700e6430dc4d14de3ca01b5
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2025-12-11 23:27:44 +0530

    Form Validation

--- JavaScript/FormValidation/script.js
+++ JavaScript/FormValidation/script.js
@@ -1,48 +1,63 @@
 function submit() {
-  const nm = document.getElementById("fullName").value.trim();
+  const nm = document.getElementById("fullname").value.trim();
   const em = document.getElementById("email").value.trim();
-  const ph = document.getElementById("number").value.trim();
+  const ph = document.getElementById("phone").value.trim();
   const db = document.getElementById("dob").value.trim();
 
-
-
-  document.querySelectorAll(".Error").forEach((element)=>{
-    element.innerHTML="";
-  })
+  document.querySelectorAll(".Error").forEach((element) => {
+    element.innerHTML = "";
+  });
 
   //Validation start
-  if (!/^[A-Za-z ]+$/.test(nm)) {
-    // console.log("wrong name");
-    document.getElementById("NameError").innerText="Only alphabet d spaces are allowed";
+  if (!nm) {
+    document.getElementById("NameError").innerText = "Required";
+    return;
+  } else if (!/^[A-Za-z ]+$/.test(nm)) {
+    document.getElementById("NameError").innerText =
+      "Only Alphabets and Spaces are Allowed";
+    return;
+  }
+  if (!em) {
+    document.getElementById("EmailError").innerText = "Required";
+    return;
+  } else if (!/^[\w\.]+@(gmail|outlook|ricr|yahoo)\.(com|in|co.in)$/.test(em)) {
+    document.getElementById("EmailError").innerText = "Use Proper Email Format";
     return;
   }
 
-  if (!/^[\w\.]+@(gmail|outlook|ricr)\.(com|in|co.in)$/.test(em)) {
-    // console.log("wrong email");
-    document.getElementById("EmailError").innerText="Use proper email format";
+  if (!ph) {
+    document.getElementById("PhoneError").innerText = "Required";
+    return;
+  } else if (!/^[6-9]\d{9}$/.test(ph)) {
+    document.getElementById("PhoneError").innerText =
+      "Only Indian Mobile Nummber allowed";
     return;
   }
 
-  if (!/^[6-9]\d{9}$/.test(ph)) {
-    // console.log("wrong number");/
-    document.getElementById("NameError").innerText="Only alphabet d spaces are allowed";
+  //   const currentDate=new Date();
+  //  console.log(currentDate);
+
+  if (!db) {
+    document.getElementById("dob").innerText = "Required";
     return;
+  } else {
+    const currentyear = new Date().getFullYear();
+    const birthyear = Number(db.split("-")[0]);
+    if (currentyear - birthyear < 17) {
+      document.getElementById("DOBError").innerText =
+        "You must be 18 years Old";
+      return;
+    }
   }
 
-      const currentDate=new Date();
-     console.log(currentDate);
+  //   //    use calculator age and dont allow less than 18 years age
+  //   const currYear = new Date().getFullYear;
+  //   const birthYear = Number(db.split("-")[0]);
+  //   if (currYear - birthYear < 17) {
+  // alert("not eligible by age");
 
-
-//   //    use calculator age and dont allow less than 18 years age
-//   const currYear = new Date().getFullYear;
-//   const birthYear = Number(db.split("-")[0]);
-//   if (currYear - birthYear < 17) {
-    // alert("not eligible by age");
-
-//     return;
-//   }
-
-  
+  //     return;
+  //   }
 
   const data = {
     fullName: nm,
