48b08579e130a626ea956dd5f94605d70bd6ca97
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2025-12-13 20:27:12 +0530

    Jokes k pitara

--- PROJECTS/JokesApp/script.js
+++ PROJECTS/JokesApp/script.js
@@ -1,9 +1,8 @@
-async function GetNewJokes(){
-
-    const response=await fetch("https://official-joke-api.appspot.com/jokes/random");
-    const data = await response.json();
-    document.getElementById("setup").innerText=data.setup;
-    document.getElementById("punchline").innerText=data.punchline;
-    
-    
+async function GetNewJokes() {
+  const response = await fetch(
+    "https://official-joke-api.appspot.com/jokes/random"
+  );
+  const data = await response.json();
+  document.getElementById("setup").innerText = data.setup;
+  document.getElementById("punchline").innerText = data.punchline;
 }
