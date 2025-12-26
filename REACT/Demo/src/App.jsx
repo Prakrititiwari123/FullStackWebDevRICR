aa51b9ff1b9c82c9eb9c9b3463ee611242e081eb
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2025-12-26 19:35:55 +0530

    functional component of react in header

--- REACT/Demo/src/App.jsx
+++ REACT/Demo/src/App.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 id="head">
-        <i className="bi bi-x-octagon-fill"></i>Hello World
-      </h1>
+      <Header />
       <div className="bg-primary text-light">This is my first React App</div>
+      <Header />
     </>
   );
 }
---
+++ REACT/Demo/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Header = () => {
+  return (
+    <>
+      <h1 id="head">
+        <i className="bi bi-x-octagon-fill"></i>Hello World
+      </h1>
+    </>
+  );
+};
+export default Header;
