5b8e92ca523a689f0d6e70dbb5532218a1cd522e
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2025-12-26 19:45:16 +0530

    functional component (footer) in react

--- REACT/Demo/src/App.jsx
+++ REACT/Demo/src/App.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,70 @@ function App() {
       <Header />
       <div className="bg-primary text-light">This is my first React App</div>
       <Header />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto eum
+        necessitatibus suscipit nobis molestias natus doloribus? Qui,
+        repudiandae doloribus omnis perferendis voluptatem et nisi cupiditate
+        possimus necessitatibus autem quo eum neque alias ullam at esse totam.
+        Quibusdam perferendis est aspernatur a asperiores! Reprehenderit
+        perspiciatis consectetur sint eos minus labore voluptatem? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Obcaecati laborum odit non
+        est illo totam esse! Aut omnis doloremque facilis cum similique unde
+        temporibus veniam a necessitatibus. Possimus quis, laudantium vero
+        labore eligendi corporis sint nam ullam corrupti quasi eaque aperiam
+        repellat eum tempora dolor totam ipsum numquam porro quas. Fugiat
+        voluptatum expedita, eos explicabo dolor qui minus dignissimos incidunt
+        nisi earum minima consequatur soluta, ullam deleniti cumque possimus
+        tempora. Quae laboriosam at magni vitae accusantium sint reiciendis
+        excepturi voluptates ipsam fugit minima eligendi, soluta dolorum
+        suscipit tempora odio nam nesciunt officiis ab eaque. Earum alias dolore
+        officiis. Doloremque in nostrum delectus adipisci amet fuga tempora,
+        porro ut quidem cumque quis veniam consequatur voluptates officiis!
+        Atque ab ut architecto, ratione voluptates corrupti, voluptatem odio
+        aliquam saepe voluptatibus temporibus, aut quasi? Ratione voluptatibus
+        labore nam maxime natus temporibus quam in? Laudantium obcaecati
+        molestias eligendi nostrum voluptates repellendus, quod corporis quae
+        harum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium eum
+        quaerat facere sed voluptatum dolorem temporibus optio incidunt? Iure
+        obcaecati beatae, nihil aliquam quasi eum facilis eligendi, modi quas
+        optio illum tempore impedit repellat reprehenderit? Magni repudiandae
+        accusantium dolorum voluptatem minus mollitia quisquam cumque, fugiat
+        saepe. Numquam pariatur maiores asperiores sit unde consectetur. Numquam
+        nisi excepturi porro adipisci fugiat autem quaerat iusto vel non facere
+        sunt amet, placeat fugit beatae sit veniam eius officia perspiciatis
+        voluptates deserunt libero? Asperiores aut dicta explicabo magnam
+        placeat excepturi, ipsam dolorum eaque sint culpa aliquid qui voluptatum
+        quis doloremque assumenda quas architecto ea, cumque perspiciatis
+        quibusdam vitae quae! Nisi maxime quis unde enim amet perspiciatis
+        praesentium obcaecati quia consequatur perferendis aut illum nesciunt
+        consectetur repudiandae ipsum ab soluta sunt ex illo doloremque,
+        deserunt dolor totam. Delectus iste aliquam ipsum aut commodi ex modi
+        hic sapiente officia? Nihil totam tempore explicabo quasi. Ipsum unde
+        adipisci est quis laudantium natus distinctio culpa provident numquam
+        ab? Eaque hic unde dolores ab officiis cumque eius harum, voluptate
+        beatae est provident libero possimus vitae fugiat nobis natus quas alias
+        earum perspiciatis fuga similique? Praesentium architecto consequatur
+        cum sint voluptatum, perspiciatis vitae quos incidunt aut laboriosam
+        qui, reprehenderit commodi cumque!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+        molestias repellendus velit deleniti. Ab laudantium itaque ipsam, illo
+        hic fugit quisquam amet, tempore aspernatur aliquid dolore iusto neque
+        alias eos modi accusantium. Deserunt laudantium vitae recusandae eaque,
+        sapiente odio modi quia distinctio, adipisci molestias atque corporis
+        rerum assumenda doloremque voluptas consequatur nostrum vero veritatis
+        eveniet earum totam quasi? Sit, perferendis ullam. Veniam omnis,
+        consequatur architecto saepe velit fuga, porro atque blanditiis dolorem
+        officia ut inventore quis officiis! Nostrum inventore iure voluptatibus
+        vitae commodi.lorem
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, maxime?
+        Lorem ipsum dolor sit amet.
+      </p>
+      <Footer />
     </>
   );
 }
---
+++ REACT/Demo/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <>
+      <div className="d-flex justify-content-evenly bg-secondary text-light">
+        <span>All rights reserved</span>
+        <span>Copyright @2025</span>
+        <span>Made with ❤️ by Prakriti</span>
+      </div>
+    </>
+  );
+};
+export default Footer;
