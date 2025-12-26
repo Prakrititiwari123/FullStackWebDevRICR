e30cb017f258bfc5903118ba8dab592d9dd98a27
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2025-12-26 19:29:46 +0530

    react by bootstrap

--- REACT/Demo/src/App.jsx
+++ REACT/Demo/src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
-import "bootstrap-icons/font/bootstrap-icons.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
-  
- 
-
   return (
     <>
-      <h1 id="head"><i className="bi bi-x-octagon-fill"></i>Hello World</h1>
+      <h1 id="head">
+        <i className="bi bi-x-octagon-fill"></i>Hello World
+      </h1>
       <div className="bg-primary text-light">This is my first React App</div>
     </>
   );
