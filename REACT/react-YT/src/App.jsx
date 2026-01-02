8be9f699381153cdbfb555ef2ec74a0a038e0d4b
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-01-02 16:30:55 +0530

    feat: Add User component and integrate it into App; update rendering logic for Cards

--- REACT/react-YT/src/App.jsx
+++ REACT/react-YT/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 import Cards from "./components/Cards";
+import User from "./components/User"
 
 const App = () => {
+  const arr=[10,20,30]
   return (
     <>
       <div className="parents">
-        
+        {arr.map(function(e){
+          return <Cards/>
+        })}
       </div>
     </>
   );
