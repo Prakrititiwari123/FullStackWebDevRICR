7075ec2ada8794ab478e980d558f7b1143a24eed
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-01-03 14:11:47 +0530

    feat: Add user age display and update functionality in App component

--- REACT/UseStateHook/src/App.jsx
+++ REACT/UseStateHook/src/App.jsx
@@ -42,13 +42,33 @@ const App = () => {
   {
     setCount (count+5)
   }
+
+  const [num, setNum] = useState({user:'Abc',age:20})
+  const clickMe=()=>{
+    const newNum={...num}
+    newNum.user='Pqr'
+    newNum.age=30
+    setNum(newNum)
+  }
+
+
+
   return (
+    <>
     <div className=' '>
       <h1 className=' h-20 w-20 border p-7 ps-8 m-18 rounded-2xl bg-pink-300'>{count}</h1>
       <button className='border p-2 mx-5 my-5 rounded bg-blue-400 hover:bg-blue-900 hover:text-white' onClick={increase}>increase</button>
       <button className='border p-2 mx-5 my-5 rounded bg-blue-400 hover:bg-blue-900 hover:text-white'onClick={decrease}>decrease</button>
       <button className='border p-2 mx-5 my-5 rounded bg-blue-400 hover:bg-blue-900 hover:text-white'onClick={jump5}>jump by 5</button>
     </div>
+ 
+    <div className='border p-5  h-30 w-30 text-center m-15'>
+    <h1>{num.user},{num.age}</h1>
+    <button className='border p-2 mx-5 my-5 rounded bg-amber-400 hover:bg-blue-900 hover:text-white' onClick={clickMe}>Click</button>
+    </div>
+    
+    
+    </>
   )
 }
 
