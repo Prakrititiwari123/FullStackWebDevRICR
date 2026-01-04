3334238a3b49effd51c71bd16f0583d8dad20c0a
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-01-04 23:42:32 +0530

    Add user data handling in LocalStorage component

--- REACT/react-YT/src/components/LocalStorage.jsx
+++ REACT/react-YT/src/components/LocalStorage.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 
 const LocalStorage = () => {
+
+  const user1={
+    username:"abx",
+    age:22,
+    city:"bhopal"
+
+  }
+
+  // localStorage.setItem('user','sarthak')
+   const user = localStorage.getItem('user');
+  console.log(user);
+  localStorage.setItem('user1',JSON.stringify(user1))
+  const back=JSON.parse(localStorage.getItem('user1'))
+  console.log(back);
+  
+  
+
   return <div>LocalStorage</div>;
 };
 
