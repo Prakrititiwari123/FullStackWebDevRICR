0ae144727f1f8f036cf9be78f473baf9d4bf4455
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-01-08 00:45:00 +0530

    Implement animated navigation in Header component

--- REACT/PORTFOLIO/src/components/Header.jsx
+++ REACT/PORTFOLIO/src/components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <motion.nav 
+    initial={{ y:-50, opacity:0, backdropFilter:"blur(0px)"}}
+    animate={{y:0, opacity:1, backdropFilter:"blur(20px)"}}
+    transition={{
+      duration: 0.6,
+      ease: "easeInOut"
+    }
+    } 
+    className='fixed w-full z-50 shadow-xl bg-white/10 dark:bg-black/10'
+    >
+
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4'>
+
+    </div>
+
+    </motion.nav>
+    
+    </>
   )
 }
 
