ed18c7753ffecab8b5eeb782f8d6ff5889333031
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-01-10 20:33:30 +0530

    Refactor error handling in user authentication and update routes for improved clarity and consistency

--- NODE/FirstBackend/src/controllers/myController.js
+++ NODE/FirstBackend/src/controllers/myController.js
@@ -1,13 +1,23 @@
 import User from "../models/userModel.js";
 
-export const UserRegister = async (req, res) => {
+export const UserRegister = async (req, res, next) => {
   try {
     const { fullName, email, phone, password } = req.body;
 
     if (!fullName || !email || !phone || !password) {
-      res.status(400).json({ message: "All fields required" });
-      return;
+      // res.status(400).json({ message: "All fields required" });
+      const error = new Error(" All fields required");
+      error.statusCode = 400;
+      return next(error);
     }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error(" Email already exists");
+      error.statusCode = 409;
+      return next(error);
+    }
+
     const newUser = await User.create({
       fullName,
       email,
@@ -20,43 +30,82 @@ export const UserRegister = async (req, res) => {
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal server error" });
+    // res.status(500).json({ message: "Internal server error" });
+    next(error);
   }
 };
 
-export const UserLogin = async (req, res) => {
+export const UserLogin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      res.status(400).json({ message: "All fields required" });
-      return;
+      const error = new Error(" User not found");
+      error.statusCode = 404;
+      return next(error);
     }
-    const existingUser = await User.find({ email });
+    const existingUser = await User.findOne({ email });
     if (!existingUser) {
-      res.status(404).json({ message: "User not found " });
-      return;
+      const error = new Error(" User not found");
+      error.statusCode = 401;
+      return next(error);
     }
 
-    // const isVerified = password === existingUser.password;
-    if (!password === existingUser.password) {
-      res.status(402).json({ message: "User not authorized" });
-      return;
+    const isVerified = password === existingUser.password;
+    if (!isVerified) {
+      const error = new Error(" User not authorized");
+      error.statusCode = 402;
+      return next(error);
+      // res.status(402).json({ message: "User not authorized" });
+      // return;
     }
     console.log(existingUser);
 
     res.status(200).json({ message: "Welcome back", data: existingUser });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal server error" });
+    // res.status(500).json({ message: "Internal server error" });
+    next(error);
   }
 };
 
-export const UserLogout = async (req, res) => {
+export const UserLogout = async (req, res, next) => {
   try {
     res.status(200).json({ message: "logout successfull" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal server error" });
+    next(error);
+    // res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const UserUpdate = async (req, res, next) => {
+  try {
+    const { fullName, email, phone } = req.body;
+
+    if (!fullName || !email) {
+      const error = new Error(" All fields required");
+      error.statusCode = 400;
+      return next(error);
+    }
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      const error = new Error(" User not found");
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    existingUser.fullName = fullName;
+    existingUser.phone = phone;
+
+    await existingUser.save();
+
+    res
+      .status(200)
+      .json({ message: "User updated successfully", data: existingUser });
+
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
 };
--- NODE/FirstBackend/src/routers/myRouter.js
+++ NODE/FirstBackend/src/routers/myRouter.js
@@ -1,10 +1,16 @@
-import express from 'express'
-import { UserRegister,UserLogin,UserLogout } from '../controllers/myController.js';
-const router= express.Router();
+import express from "express";
+import {
+  UserRegister,
+  UserLogin,
+  UserLogout,
+  UserUpdate,
+} from "../controllers/myController.js";
+const router = express.Router();
 
 router.post("/register", UserRegister);
 router.post("/login", UserLogin);
 router.get("/logout", UserLogout);
 
-export default router;
+router.put("/update", UserUpdate);
 
+export default router;
