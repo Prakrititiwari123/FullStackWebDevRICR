c7bfa076e00d441b7c68dcd1ad49361dfbb64c31
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-01-15 02:37:27 +0530

    formatting app.jsx of craving

--- Cravings/client/src/App.jsx
+++ Cravings/client/src/App.jsx
@@ -8,12 +8,11 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { Toaster } from "react-hot-toast";
 
-
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <Toaster/>
+        <Toaster />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
