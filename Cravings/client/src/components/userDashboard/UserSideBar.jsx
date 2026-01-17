1d4878bac5114d16ebaa27aaf86f5a6a9065986c
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-01-17 20:25:01 +0530

    Hmburger and collapse of sidebar

--- Cravings/client/src/components/userDashboard/UserSideBar.jsx
+++ Cravings/client/src/components/userDashboard/UserSideBar.jsx
@@ -6,72 +6,92 @@ import { TbTransactionRupee } from "react-icons/tb";
 import { RiCustomerService2Fill } from "react-icons/ri";
 import { GiHamburgerMenu } from "react-icons/gi";
 
-const UserSideBar = ({ active, setActive }) => {
+const UserSideBar = ({ active, setActive, isCollapsed, setIsCollapsed }) => {
+  const menuItems = [
+    { key: "overview", title: "Overview", icon: <TbChartTreemap /> },
+    { key: "profile", title: "Profile", icon: <CgProfile /> },
+    { key: "orders", title: "Orders", icon: <TiShoppingCart /> },
+    { key: "transaction", title: "Transaction", icon: <TbTransactionRupee /> },
+    { key: "helpdesk", title: "Help Desk", icon: <RiCustomerService2Fill /> },
+  ];
   return (
     <>
       <div className=" p-3">
-        <div className="text-xl font-bold flex items-center gap-2"> <GiHamburgerMenu/>User Dashboard</div>
-        <hr />
-        <div className=" grid gap-3 p-6">
+        <div className="h-10 text-xl font-bold flex items-center gap-5 mb-2">
+          {" "}
           <button
-            className={`flex gap-3 items-center p-3 rounded-xl 
-            ${
-              active === "overview"
-                ? "bg-(--color-secondary) text-white"
-                : "hover:bg-gray-100/70"
-            }`}
-            onClick={() => setActive("overview")}
+            className=" ms-2 hover:scale-105"
+            onClick={() => setIsCollapsed(!isCollapsed)}
           >
-            <TbChartTreemap />
-            Overview
+            <GiHamburgerMenu className="" />
           </button>
-          <button
-            className={`flex gap-3 items-center p-3 rounded-xl ${
-              active === "profile"
-                ? "bg-(--color-secondary) text-white"
-                : "hover:bg-gray-100/70"
-            }`}
-            onClick={() => setActive("profile")}
-          >
-            {" "}
-            <CgProfile />
-            Profile
-          </button>
-          <button
-            className={`flex gap-3 items-center p-3 rounded-xl ${
-              active === "orders"
-                ? "bg-(--color-secondary) text-white"
-                : "hover:bg-gray-100/70"
-            }`}
-            onClick={() => setActive("orders")}
-          >
-            {" "}
-            <TiShoppingCart />
-            Orders
-          </button>
-          <button
-            className={`flex gap-3 items-center p-3 rounded-xl ${
-              active === "transaction"
-                ? "bg-(--color-secondary) text-white"
-                : "hover:bg-gray-100/70"
-            }`}
-            onClick={() => setActive("transaction")}
-          >
-            <TbTransactionRupee />
-            Transactions
-          </button>
-          <button
-            className={`flex gap-3 items-center p-3 rounded-xl ${
-              active === "helpdesk"
-                ? "bg-(--color-secondary) text-white"
-                : "hover:bg-gray-100/70"
-            }`}
-            onClick={() => setActive("helpdesk")}
-          >
-            <RiCustomerService2Fill />
-            Help Desk
-          </button>
+          {!isCollapsed && (
+            <span className="overflow-hidden text-nowrap">User Dashboard</span>
+          )}
         </div>
+        <hr />
+
+        {/* {isCollapsed ? (
+          <div className="py-6 space-y-5 ">
+            {menuItems.map((item, idx) => (
+              <button
+                className={`flex gap-3 items-center p-3 rounded-xl 
+            ${
+              active === item.key
+                ? "bg-(--color-secondary) text-white"
+                : "hover:bg-gray-100/70"
+            }`}
+                onClick={() => setActive(item.key)}
+                key={idx}
+              >
+                {item.icon}
+              </button>
+            ))}
+          </div>
+        ) : (
+          <div className=" py-6 space-y-3">
+            {menuItems.map((item, idx) => (
+              <button
+                className={`flex gap-3 items-center p-3 rounded-xl 
+            ${
+              active === item.key
+                ? "bg-(--color-secondary) text-white"
+                : "hover:bg-gray-100/70"
+            }`}
+                onClick={() => setActive(item.key)}
+                key={idx}
+              >
+                {item.icon}
+                {item.title}
+              </button>
+            ))}
+          </div>
+        )} */}
+
+
+
+
+        <div className=" py-6 space-y-5 w-full ">
+            {menuItems.map((item, idx) => (
+              <button
+                className={`flex gap-3 items-center text-lg ps-2 rounded-xl h-10 w-full text-nowrap overflow-hidden duration-300
+            ${
+              active === item.key
+                ? "bg-(--color-secondary) text-white"
+                : "hover:bg-gray-100/70"
+            }`}
+                onClick={() => setActive(item.key)}
+                key={idx}
+              >
+                {item.icon}
+                {!isCollapsed && item.title}
+              </button>
+            ))}
+          </div>
+
+
+
+
       </div>
     </>
   );
