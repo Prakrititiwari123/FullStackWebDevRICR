68c9db092f931601e673d0884e00b3e8c8188de6
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-01-18 04:20:44 +0530

    login page of connecta

--- chat-app/client/src/pages/LoginPage.jsx
+++ chat-app/client/src/pages/LoginPage.jsx
@@ -9,26 +9,34 @@ const LoginPage = () => {
   const [bio, setBio] = useState("");
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
-  const onSubmitHandler=(event)=>{
+  const onSubmitHandler = (event) => {
     event.preventDefault();
 
-    if(currState === "Sign up" && !isDataSubmitted){
-      setIsDataSubmitted(true)
+    if (currState === "Sign up" && !isDataSubmitted) {
+      setIsDataSubmitted(true);
       return;
     }
-  }
+  };
 
   return (
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
       {/* left */}
       <img src={assets.logo_big} alt="" className="w-[min(30vw,250px)]" />
       {/* right */}
-      <form onSubmit={onSubmitHandler} className=" border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
-        
+      <form
+        onSubmit={onSubmitHandler}
+        className=" border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg"
+      >
         <h2 className=" font-medium text-2xl flex justify-between items-center">
           {currState}
-          {isDataSubmitted && <img onClick={()=> setIsDataSubmitted(false)} src={assets.arrow_icon} alt="" className="w-5 cursor-pointer" />}
-          
+          {isDataSubmitted && (
+            <img
+              onClick={() => setIsDataSubmitted(false)}
+              src={assets.arrow_icon}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          )}
         </h2>
 
         {currState === "Sign up" && !isDataSubmitted && (
@@ -90,14 +98,23 @@ const LoginPage = () => {
           {currState === "Sign up" ? (
             <p className="text-sm text-gray-600">
               Already have an account?{" "}
-              <span className="font-medium text-violet-500 cursor-pointer" onClick={()=>{setCurrState("Login");setIsDataSubmitted(false)}}>
+              <span
+                className="font-medium text-violet-500 cursor-pointer"
+                onClick={() => {
+                  setCurrState("Login");
+                  setIsDataSubmitted(false);
+                }}
+              >
                 Login Here
               </span>
             </p>
           ) : (
             <p className="text-sm text-gray-600">
               Create an account{" "}
-              <span className="font-medium text-violet-500 cursor-pointer" onClick={()=>setCurrState("Sign up")}>
+              <span
+                className="font-medium text-violet-500 cursor-pointer"
+                onClick={() => setCurrState("Sign up")}
+              >
                 Click Here
               </span>
             </p>
