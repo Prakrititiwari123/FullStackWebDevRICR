21053d5cf61d1e6e1b30c9992b2f83720630fa5c
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-01-21 02:33:40 +0530

    Backend of connecta

--- chat-app/server/controllers/messageController.js
+++ chat-app/server/controllers/messageController.js
@@ -1,5 +1,7 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
+import { io, userSocketMap } from "../server.js";
 
 export const getUserForSidebar = async (req, res) => {
   try {
@@ -52,9 +54,41 @@ export const getMessages = async (req, res) => {
 
 export const markMessageAsSeen = async (req, res) => {
   try {
-    const { id}=req.params
-    await Message.findByIdAndUpdate(id, {seen:true})
-    res.json({success:true})
+    const { id } = req.params;
+    await Message.findByIdAndUpdate(id, { seen: true });
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    // emit the new mesg to the receiver socket
+    const receiverSocketId = userSocketMap[receiverId];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    res.json({ success: true, newMessage });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
