7e55f6fa47412f60011f9722bbf6e19ff4858e44
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-01-29 14:59:06 +0530

    Foodie-Frenzy in progress

--- FoodieFrenzy/frontend/src/App.jsx
+++ FoodieFrenzy/frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home'
+import ContactPage from './pages/ContactPage/ContactPage'
+import AboutPage from './pages/AboutPage/AboutPage'
+import Cart from './pages/Cart/Cart'
+import Menu from './pages/Menu/Menu'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/contact' element={<ContactPage/>}/>
+      <Route path='/about' element={<AboutPage/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/menu' element={<Menu/>}/>
     </Routes>
   )
 }
